16d92c64aeb4fec0c8370a4344d87cb9e458af31
Piercecyl <37281036+Piercecyl@users.noreply.github.com>
2018-10-29 13:43:40 +0800

    Update Machine learning/Regression/Best Fit Slope.py
    R Square

--- Machine learning/Regression/Best Fit Slope.py
+++ Machine learning/Regression/Best Fit Slope.py
@@ -31,4 +31,22 @@ for x in xs:
 plt.scatter(xs ,ys ,color = '#003F72')
 # y = m*x + b，這邊將畫出input = x ， output = y的圖
 plt.plot(xs ,regression_line)
-plt.show()
+#plt.show()
+
+
+#R^2：判斷Regression的合適度
+#(y - y^)總和
+def squared_error(ys_orig,ys_line):
+    return sum((ys_line - ys_orig) * (ys_line - ys_orig))
+
+#公式推導
+def coefficient_of_determination(ys_orig,ys_line):
+    
+    y_mean_line = [mean(ys_orig) for y in ys_orig]
+    squared_error_regr = squared_error(ys_orig, ys_line)
+    squared_error_y_mean = squared_error(ys_orig, y_mean_line)
+    return 1 - (squared_error_regr/squared_error_y_mean)
+
+r_squared = coefficient_of_determination(ys,regression_line)
+print(r_squared)
+
